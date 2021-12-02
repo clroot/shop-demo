00a89ad71ee4a1e899ae28e58a0e4151965c6226
clroot <abcdkh1209@gmail.com>
2021-12-02 16:22:42 +0900

    initialize project

---
+++ src/test/java/com/ssunivs/shopdemo/ShopDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ssunivs.shopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
