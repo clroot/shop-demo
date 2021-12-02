8ed44062d37fa79dd061700c1998cde1224aa4f3
clroot <abcdkh1209@gmail.com>
2021-12-02 19:18:03 +0900

    test: Jpa Auditing 활성화 테스트

--- src/test/java/com/ssunivs/shopdemo/ShopDemoApplicationTests.java
+++ src/test/java/com/ssunivs/shopdemo/ShopDemoApplicationTests.java
@@ -1,12 +1,40 @@
 package com.ssunivs.shopdemo;
 
+import com.ssunivs.shopdemo.domain.BaseEntity;
+import com.ssunivs.shopdemo.domain.Member;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Transactional
 class ShopDemoApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    @DisplayName("JPA Auditing 테스트")
+    public void japAuditingTest() {
+        //given
+        BaseEntity baseEntity = Member.builder().build();
+
+        //when
+        em.persist(baseEntity);
+
+        //then
+        assertTrue(LocalDateTime.now().isAfter(baseEntity.getCreatedAt()));
+        assertTrue(LocalDateTime.now().isAfter(baseEntity.getUpdatedAt()));
     }
 }
